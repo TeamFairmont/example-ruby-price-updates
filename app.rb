1335177f9edc6cb930ae6e48bae526f41e3e2d77
Brendan L <drac@dracsoft.com>
2016-02-08 13:45:00 -0500

    demo v1, nearly ready

--- app.rb
+++ app.rb
@@ -6,25 +6,33 @@ require 'net/http'
 require 'uri'
 
 class App
-  def call(env)
-    request = Rack::Request.new env
-    response = Rack::Response.new
+    def call(env)
+        request = Rack::Request.new env
+        response = Rack::Response.new
 
-    case request.path
-    when "/get-updates"
+        case request.path
+            when "/get-updates"
+                getUpdates(request, response)
+        end
+
+        response.status = 200
+        response.finish
+    end
+
+    def getUpdates(request, response)
         key = 'another_hmac_key'
         group = 'enduser'
-        resp = '{"since":1999999}'
+        req = '{"since":'+request.params['since']+'}'
         data={}
         data['timestamp']=Time.now.to_i.to_s
-        data['message']=resp
+        data['message']=req
         digest = OpenSSL::Digest.new('sha512')
 
         hmacsig = Base64.encode64(OpenSSL::HMAC.hexdigest(digest, key, JSON.generate(data)))
-        resp64 = Base64.encode64(JSON.generate(data))
+        req64 = Base64.encode64(JSON.generate(data))
         pdata={}
         pdata['signature']=hmacsig
-        pdata['data']=resp64
+        pdata['data']=req64
 
         uri = URI.parse("http://localhost:8888/request/products/get-updated")
 
@@ -34,13 +42,14 @@ class App
         request.body=JSON.generate(pdata)
         engineres = http.request(request)
 
-        response.write JSON.generate(JSON.parse(engineres.body)['return_value'])
+        #just return the results directly
+        #TODO check for errors, timeouts, etc
+        v=JSON.parse(engineres.body)
+        if v['complete']
+            response.write JSON.generate(v['return_value'])
+        else
+            #TODO create a fetch loop for get the complete result
+            response.write '{"products":[{"sku":"QWEQWE", "price":0, "inventory":0}]}'
+        end
     end
-
-    response.status = 200
-
-    response.finish
-  end
-
-
 end
--- worker.rb
+++ worker.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bunny'
 require 'json'
+require 'sqlite3'
+require 'base64'
 
 conn = Bunny.new
 conn.start
@@ -11,32 +13,81 @@ qfc  = ch.queue("formatContent", :durable=>true, :autodelete=>false)
 qfp  = ch.queue("fluctuatePrice", :durable=>true, :autodelete=>false)
 x  = ch.default_exchange
 
+#setup db
+db = SQLite3::Database.new( "./demo.db" )
+prng = Random.new
+
+#loop on workers
 while true do
-    qfp.subscribe do |delivery_info, metadata, payload|
-        pl = JSON.parse(payload)
-        #change prices and inv around in db
-        pl['data']['success']=true
-        out=JSON.generate(pl)
-        puts out
 
-        x.publish(out, :routing_key=>metadata.reply_to, :correlation_id=>metadata.correlation_id)
-    end
-
+    #FORMAT CONTENT TO 'STANDARD' OF ALL CAPS
     qfc.subscribe do |delivery_info, metadata, payload|
+        puts "formatContent"
         pl = JSON.parse(payload)
-        #TODO change .sku fr each product to uppercase
-        out=JSON.generate(pl)
-        puts out
 
+        #change .sku for each product to uppercase
+        pl["return_value"]["products"].each_with_index do |value, key|
+            pl["return_value"]["products"][key]['sku'] = pl["return_value"]["products"][key]['sku'].upcase
+            pl["return_value"]["products"][key]['price'] = pl["return_value"]["products"][key]['price'].round(2)
+        end
+
+        #publish response back to mq
+        out=JSON.generate(pl)
         x.publish(out, :routing_key=>metadata.reply_to, :correlation_id=>metadata.correlation_id)
     end
 
+    #GET PRODUCTS UPDATED ON OR AFTER 'SINCE'
     qgu.subscribe do |delivery_info, metadata, payload|
+        puts "getUpdated"
         pl = JSON.parse(payload)
 
-        out=JSON.generate(pl)
-        puts out
+        #fetch updated product info from db
+        rows=[]
+        if pl["initial_input"]["since"].to_i == 0
+            rows = db.execute( "SELECT * FROM productlog GROUP by sku order by stamp desc" )
+        else
+            rows = db.execute( "SELECT * FROM productlog WHERE stamp >= ?", pl["initial_input"]["since"].to_i/1000 )
+        end
 
+        data=[]
+        rows.each do |row|
+            data << {
+                :sku => row[0],
+                :price => row[1],
+                :inventory => row[2]
+            }
+        end
+        pl['return_value']['products']=data
+
+        #truncate price log to last 200 if we have more than 5000 entries
+        count = db.get_first_value("select count(*) from productlog")
+        if count > 5000
+            db.execute("delete from productlog where stamp not in (select stamp from productlog order by stamp desc limit 200)")
+        end
+
+        #publish response back to mq
+        out=JSON.generate(pl)
+        x.publish(out, :routing_key=>metadata.reply_to, :correlation_id=>metadata.correlation_id)
+    end
+
+    #FLUCTUATE PRICE AND INVENTORY
+    qfp.subscribe do |delivery_info, metadata, payload|
+        puts "fluctuatePrice"
+        pl = JSON.parse(payload)
+
+        #change prices and inv around in db
+        inv = prng.rand(100)-25
+        price = prng.rand()*200
+        sku = "P"+Base64.encode64(prng.rand(101..200).to_s).downcase.reverse.sub("\n","")
+        db.execute("INSERT INTO productlog VALUES (?,?,?,strftime('%s','now'))",
+            sku, price, inv
+        )
+
+        #set return val
+        pl['return_value']['success']=true
+
+        #publish response back to mq
+        out=JSON.generate(pl)
         x.publish(out, :routing_key=>metadata.reply_to, :correlation_id=>metadata.correlation_id)
     end
 
